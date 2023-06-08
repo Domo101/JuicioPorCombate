82c47f9d6a0dba677943e746022692c0b1809f0b
David Coello Pulido <davidcoello@eprosima.com>
2023-06-08 21:22:09 +0200

    Cargar jugador

--- src/juicio_por_combate.py
+++ src/juicio_por_combate.py
@@ -20,4 +20,26 @@ def guardar_jugador(fichero, jugador):
     archivo.close()
 
 
+def cargar_jugador():
+    """cargar la jugador desde un fichero"""
+    try:
+        archivo = open("bd.json", "r")
+
+        # leemos el archivo
+        jugador_en_json = archivo.read()
+
+        # pasar el string a un array
+        jugador = json.loads(jugador_en_json)
+
+        # cerramos el archivo
+        archivo.close()
+
+    except FileNotFoundError:
+        print("No se ha encontrado bd.json, creando nuevo jugador")
+        return {}
+
+    # retornamos el array de la jugador
+    return jugador
+
+
 print("Juicio por combate")

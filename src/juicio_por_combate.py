5a2eddc00c1529b7f519cfc0275aa293d67ed274
David Coello Pulido <davidcoello@eprosima.com>
2023-06-08 21:02:12 +0200

    Guardar jugador

--- src/juicio_por_combate.py
+++ src/juicio_por_combate.py
@@ -1,1 +1,23 @@
+import json
+
+
+def guardar_jugador(jugador):
+    """guarda la jugador en un fichero"""
+    # escribir en un archivo
+
+    # w: para escribir en el
+    # r: para leer
+    # +: para crearlo si no existe
+    archivo = open("./bd.json", "w+")
+
+    # pasamos la jugador a un string en formato json
+    jugador_en_json = json.dumps(jugador, indent=2)
+
+    # lo enchuflamos al archivo
+    archivo.write(jugador_en_json)
+
+    # cerramos archivo
+    archivo.close()
+
+
 print("Juicio por combate")
